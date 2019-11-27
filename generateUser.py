64ff4c01ef597d5fa49e5cde706069f5b258f5a5
tecyang <me@tecyang.com>
2019-11-27 14:31:31 +0800

    增加导入用户脚本

---
+++ generateUser.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import json
+
+
+class GenerateUser:
+
+    path = os.path.dirname(__file__)
+
+    def getFileList(self):
+        files = os.listdir(self.path)
+        for file in files:
+            print(file)
+            if file.find('txt') > 0:
+                self.generateUser(file)
+
+    def generateUser(self, file):
+
+        with open(self.path + '/' + file) as f:
+            j_context = json.loads(f.read())
+            f.close()
+
+        templatePath = self.path + '/userTemplate.ldif'
+        with open(templatePath) as f:
+            template = f.read()
+            USER = template.replace("UID",j_context.get('登陆名')) \
+                .replace("SN",j_context.get('姓')) \
+                .replace("CN",j_context.get('名')) \
+                .replace("GIVEN_NAME",j_context.get('名')) \
+                .replace("DISP_NAME",j_context.get('姓') + j_context.get('名')) \
+                .replace("MAIL",j_context.get('邮箱')) \
+                .replace("PASSWORD",j_context.get('密码'))
+            print(USER)
+            f.close()
+
+            user = self.path + '/user.ldif'
+            with open(user, 'a+') as f:
+                f.write('\n' + USER + '\n')
+                f.close()
+
+
+if __name__ == "__main__":
+    generateUser = GenerateUser()
+    generateUser.getFileList()
