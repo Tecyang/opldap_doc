3dff760549a40715c3dee16041700d1da41789e3
tecyang <me@tecyang.com>
2020-05-26 12:00:50 +0800

    修改生成ldif 文件逻辑

--- generateUser.py
+++ generateUser.py
@@ -9,12 +9,14 @@ class GenerateUser:
 
     def getFileList(self):
         files = os.listdir(self.path)
+        uid_number = 2000
         for file in files:
             print(file)
             if file.find('txt') > 0:
-                self.generateUser(file)
+                self.generateUser(file,uid_number)
+                uid_number = uid_number+1
 
-    def generateUser(self, file):
+    def generateUser(self, file,uid_number=2000):
 
         with open(self.path + '/' + file) as f:
             j_context = json.loads(f.read())
@@ -29,7 +31,8 @@ class GenerateUser:
                 .replace("GIVEN_NAME",j_context.get('名')) \
                 .replace("DISP_NAME",j_context.get('姓') + j_context.get('名')) \
                 .replace("MAIL",j_context.get('邮箱')) \
-                .replace("PASSWORD",j_context.get('密码'))
+                .replace("PASSWORD",j_context.get('密码')) \
+                .replace("NUMBER",str(uid_number))
             print(USER)
             f.close()
 
