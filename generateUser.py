46ef2037ae06e40991450583b25e94fda93bdd64
tecyang <me@tecyang.com>
2021-03-08 13:02:18 +0800

    feat: adduserid

--- generateUser.py
+++ generateUser.py
@@ -10,7 +10,7 @@ class GenerateUser:
     def getFileList(self):
         files = os.listdir(self.path)
         # 生成用户的起始id
-        uid_number = 2079
+        uid_number = 2080
         for file in files:
             print(file)
             if file.find('txt') > 0:
